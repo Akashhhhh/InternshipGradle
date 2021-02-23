7c7e1a8a5171e577f09540450ca9431a27163a18
raramuri <akash.gupta@zopsmart.com>
2021-02-23 20:17:50 +0530

    Ecommerce|Akash Gupta|improve the documentation

--- src/main/java/ecommerce/cache/LruCache.java
+++ src/main/java/ecommerce/cache/LruCache.java
@@ -2,25 +2,50 @@ package ecommerce.cache;
 
 import java.util.*;
 
+/**
+ * This class is used for creating generic cache
+ * @param <key> any type of key
+ * @param <value> any type of value'
+ * @author Akash Gupta
+ */
 public class LruCache <key,value>{
     Set<key> cache;
     Map<key, value> m= new HashMap<key,value>();
     int capacity;
 
+    /**
+     * This is the constructor of LruCache
+     */
     public LruCache() {
         this.cache = new LinkedHashSet<key>(10);
         this.m = new HashMap<key,value>();
         this.capacity = 10;
     }
 
+    /**
+     * This method is used for getting the value from cache
+     * @param k key
+     * @return value in the cache
+     */
     public value get( key k) {
 
         return m.get(k);
     }
+
+    /**
+     * This method is used for deleting value from cache
+     * @param k key
+     */
     public void delete(key k){
         cache.remove(k);
 
     }
+
+    /**
+     * This method is used for checking whether key exist in the cache or not
+     * @param k key
+     * @return returns true or false
+     */
     public boolean find(key k){
         if(cache.contains(k)){
             return true;
@@ -33,6 +58,11 @@ public class LruCache <key,value>{
 //                    ", Value = " + entry.getValue());
 //    }
 
+    /**
+     * This method is used for putting value in the cache
+     * @param k key
+     * @param v value
+     */
     public void put(key k,value v) {
         if (cache.size() == capacity) {
             key firstKey = cache.iterator().next();
--- src/main/java/ecommerce/cache/LruCacheService.java
+++ src/main/java/ecommerce/cache/LruCacheService.java
@@ -5,28 +5,51 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * This is a specific lru cache for customer
+ * @param <key> key
+ * @param <value> value
+ * @author  Akash Gupta
+ */
 public class LruCacheService<key, value> {
     private static java.util.logging.Logger logger = java.util.logging.Logger.getLogger(LruCacheService.class.getName());
     Set<key> cache = null;
     Map<key, value> m = new HashMap<key, value>();
     int capacity;
 
+    /**
+     * Constructor of LruCacheService
+     */
     public LruCacheService() {
         this.cache = ((LruCache) new LruCache()).cache;
         this.m = ((LruCache) new LruCache()).m;
         this.capacity = ((LruCache) new LruCache()).capacity;
     }
 
+    /**
+     * This methodis for fetching
+     * @param k key
+     * @return it returns the uuid
+     */
     public UUID get(String k) {
 
         return (UUID) m.get(k);
     }
 
+    /**
+     * Tis method is used for deleting
+     * @param k key
+     */
     public void delete(String k) {
         cache.remove(k);
 
     }
 
+    /**
+     * This is used to check whether key exist or not
+     * @param k key
+     * @return it return true or false
+     */
     public boolean find(String k) {
         if (cache.contains(k)) {
             return true;
@@ -39,6 +62,11 @@ public class LruCacheService<key, value> {
 //                    ", Value = " + entry.getValue());
 //    }
 
+    /**
+     * This method is used for inserting into the cache
+     * @param k key
+     * @param v value
+     */
     public void put(key k, value v) {
         if (cache.size() == capacity) {
             key firstKey = cache.iterator().next();
